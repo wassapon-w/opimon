d64c46a354ae2c469906c27b11cbcd1b6eaa0411
Wassapon Watanakeesuntorn <wassapon.w@gmail.com>
2016-06-22 16:52:43 +0900

    Change from function expressions into function declarations.

--- Visualization/accessDatabase.js
+++ Visualization/accessDatabase.js
@@ -6,37 +6,44 @@ var topology = [];
 var flowMods = [];
 
 // Get flow mod messages from database
-MongoClient.connect(url, function(err, db) {
-	assert.equal(null, err);
+function getFlowMods() {
+	MongoClient.connect(url, function(err, db) {
+		assert.equal(null, err);
 
-	var cursor = db.collection('flow_mods').find();
-	cursor.each(function(err, doc) {
-		assert.equal(err, null);
-		if (doc != null) {
-			// console.dir(doc);
-			flowMods.push(doc);
-		}
-		else {
-			console.log(flowMods);
-			db.close();
-		}
+		var cursor = db.collection('flow_mods').find();
+		cursor.each(function(err, doc) {
+			assert.equal(err, null);
+			if (doc != null) {
+				// console.dir(doc);
+				flowMods.push(doc);
+			}
+			else {
+				// console.log(flowMods);
+				db.close();
+			}
+		});
 	});
-});
+}
 
 // Get topology from database
-MongoClient.connect(url, function(err, db) {
-	assert.equal(null, err);
+function getTopology() {
+	MongoClient.connect(url, function(err, db) {
+		assert.equal(null, err);
 
-	var cursor = db.collection('topology').find();
-	cursor.each(function(err, doc) {
-		assert.equal(err, null);
-		if (doc != null) {
-			// console.dir(doc);
-			topology.push(doc);
-		}
-		else {
-			console.log(topology);
-			db.close();
-		}
+		var cursor = db.collection('topology').find();
+		cursor.each(function(err, doc) {
+			assert.equal(err, null);
+			if (doc != null) {
+				// console.dir(doc);
+				topology.push(doc);
+			}
+			else {
+				console.log(topology);
+				db.close();
+			}
+		});
 	});
-});
+}
+
+getFlowMods();
+getTopology();

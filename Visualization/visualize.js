8f1eb0972b801e00356c02cac6f5c346b6d9da8b
Wassapon Watanakeesuntorn <wassapon.w@gmail.com>
2016-06-30 13:47:29 +0900

    Remove visualize part for D3 version 3.0

--- Visualization/visualize.js
+++ Visualization/visualize.js
@@ -29,52 +29,6 @@ getJSON('http://192.168.22.132:3000/topology', function(err, output){
 function visualize() {
   console.log(data);
 
-  // var width = 960,
-  //     height = 500;
-  //
-  // var color = d3.scale.category20();
-  //
-  // var force = d3.layout.force()
-  //     .charge(-120)
-  //     .linkDistance(30)
-  //     .size([width, height]);
-  //
-  // var svg = d3.select("body").append("svg")
-  //     .attr("width", width)
-  //     .attr("height", height);
-  //
-  // force
-  //     .nodes(data.switch)
-  //     .links(data.connect)
-  //     .start();
-  //
-  // var link = svg.selectAll(".link")
-  //     .data(data.connect)
-  //     .enter().append("line")
-  //     .attr("class", "link")
-  //     .style("stroke-width", function(d) { return 5 });
-  //
-  // var node = svg.selectAll(".node")
-  //     .data(data.switch)
-  //     .enter().append("circle")
-  //     .attr("class", "node")
-  //     .attr("r", 10)
-  //     .style("fill", function(d) { return color(d.switch_id); });
-  //     // .call(force.drag);
-  //
-  // node.append("title")
-  //     .text(function(d) { return d.switch_id; });
-  //
-  // force.on("tick", function() {
-  //   link.attr("x1", function(d) { return d.source.x; })
-  //       .attr("y1", function(d) { return d.source.y; })
-  //       .attr("x2", function(d) { return d.target.x; })
-  //       .attr("y2", function(d) { return d.target.y; });
-  //
-  //   node.attr("cx", function(d) { return d.x; })
-  //       .attr("cy", function(d) { return d.y; });
-  // });
-
   // var color = d3.scaleCategory20();
 
   var canvas = document.querySelector("canvas"),

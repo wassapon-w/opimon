be263c616475d61fd0c217c1fd8f63bcb99c5d1a
Wassapon Watanakeesuntorn <wassapon.w@gmail.com>
2016-07-12 16:07:36 +0900

    Testing on upload file via web page. (unfinished)

--- Visualization/settings.js
+++ Visualization/settings.js
@@ -1,16 +1,23 @@
 $(document).ready(function(){
      var i = 0;
 
-     $("#add_row").click( function(){
+     $("#add_row").click(function(){
           $('#switchName').append('<tr id="addr'+(i)+'"></tr>');
           $('#addr'+i).html("<td>" + (i+1) + "</td><td><input name='id" + i + "' type='text' placeholder='Switch ID' class='form-control input-md'  /> </td><td><input  name='name" + i + "' type='text' placeholder='Name'  class='form-control input-md'></td>");
           i++;
       });
 
       $("#delete_row").click(function(){
-        if(i >= 1){
-            $("#addr"+(i-1)).html('');
-            i--;
-        }
-    });
+          if(i >= 1) {
+              $("#addr"+(i-1)).html('');
+              i--;
+          }
+      });
+
+      $("#upload").click(function(evt) {
+          var test = $("#inputJSON").val();
+          console.log(test);
+
+          // window.open(test);
+      });
 });

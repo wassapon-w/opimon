c5c4a56a9aeb6dbc185a0fae83f7675d422d0a0f
Wassapon Watanakeesuntorn <wassapon.w@gmail.com>
2016-07-12 11:35:15 +0900

    Add sample add & delete row on settings page.

---
+++ Visualization/settings.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+     var i = 0;
+
+     $("#add_row").click( function(){
+          $('#switchName').append('<tr id="addr'+(i)+'"></tr>');
+          $('#addr'+i).html("<td>" + (i+1) + "</td><td><input name='id" + i + "' type='text' placeholder='Switch ID' class='form-control input-md'  /> </td><td><input  name='name" + i + "' type='text' placeholder='Name'  class='form-control input-md'></td>");
+          i++;
+      });
+
+      $("#delete_row").click(function(){
+        if(i >= 1){
+            $("#addr"+(i-1)).html('');
+            i--;
+        }
+    });
+});

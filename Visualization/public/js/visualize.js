80243b1a84a6ee7b80db294b5a06d0f32fb118ad
Wassapon Watanakeesuntorn <wassapon.w@gmail.com>
2016-07-26 11:17:41 +0900

    Add "idle_timeout" and "hard_timeout" on flow table.

--- Visualization/public/js/visualize.js
+++ Visualization/public/js/visualize.js
@@ -322,6 +322,8 @@ function showFlowTableByID(container, data, switch_id) {
             if(r["match"]["nw_dst"] != "0.0.0.0") { match.append($("<li/>").text("Destination IP Address : " + r["match"]["nw_dst"] + "\n")); }
             if(r["match"]["tp_src"] != "0") { match.append($("<li/>").text("Source TCP/UDP Port : " + r["match"]["tp_src"] + "\n")); }
             if(r["match"]["tp_dst"] != "0") { match.append($("<li/>").text("Destination TCP/UDP Port : " + r["match"]["tp_dst"] + "\n")); }
+            match.append($("<li/>").text("Idle Timeout : " + r["idle_timeout"]);
+            match.append($("<li/>").text("Hard Timeout : " + r["hard_timeout"]);
             row.append(match);
 
             var action = $("<td/>");
@@ -331,7 +333,7 @@ function showFlowTableByID(container, data, switch_id) {
             action.append($("<li/>").text("Max Length : " + r["actions"][0]["max_len"] + "\n"));
             row.append(action);
 
-            row.append($("<td/>").text(expireTime.toString()));
+            // row.append($("<td/>").text(expireTime.toString()));
             body.append(row);
         // }
     });

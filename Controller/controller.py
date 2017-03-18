088b8417f0eb4dcbabff64ff5536aa05706f8525
Wassapon Watanakeesuntorn <wassapon.w@gmail.com>
2017-03-18 13:42:08 +0700

    Use simple_switch.py form Ryu as a controller.

--- Controller/controller.py
+++ Controller/controller.py
@@ -2,55 +2,87 @@ from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_0
+from ryu.lib.mac import haddr_to_bin
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
 
-class L2Switch(app_manager.RyuApp):
-	def __init__(self, *args, **kwargs):
-		super(L2Switch, self).__init__(*args, **kwargs)
-		self.portTable = {}
+class SimpleSwitch(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
 
-	@set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-	def packet_in_handler(self, ev):
+    def __init__(self, *args, **kwargs):
+        super(SimpleSwitch, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
 
-		# Read packet info.
-		msg = ev.msg
-		dp = msg.datapath
-		ofp = dp.ofproto
-		ofp_parser = dp.ofproto_parser
+    def add_flow(self, datapath, in_port, dst, actions):
+        ofproto = datapath.ofproto
 
-		pkt = packet.Packet(msg.data)
-		eth = pkt.get_protocols(ethernet.ethernet)[0]
+        match = datapath.ofproto_parser.OFPMatch(
+            in_port=in_port, dl_dst=haddr_to_bin(dst))
 
-		dataPathID = dp.id
-		inPort = msg.in_port
+        mod = datapath.ofproto_parser.OFPFlowMod(
+            datapath=datapath, match=match, cookie=0,
+            command=ofproto.OFPFC_ADD, idle_timeout=0, hard_timeout=0,
+            priority=ofproto.OFP_DEFAULT_PRIORITY,
+            flags=ofproto.OFPFF_SEND_FLOW_REM, actions=actions)
+        datapath.send_msg(mod)
 
-		sourceMAC = eth.src
-		destinationMAC = eth.dst
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
 
-		self.portTable.setdefault(dataPathID, {})
-		self.portTable[dataPathID][sourceMAC] = inPort
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
 
-		if destinationMAC in self.portTable[dataPathID]:
-			outPort = self.portTable[dataPathID][destinationMAC]
-		else:
-			outPort = ofp.OFPP_FLOOD
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
+            return
+        dst = eth.dst
+        src = eth.src
 
-		self.logger.info("Switch ID : %s / Source : %s (%s) / Destination : %s (%s)", dataPathID, sourceMAC, inPort, destinationMAC, outPort)
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
 
-		actions = [ofp_parser.OFPActionOutput(outPort)]
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, msg.in_port)
 
-		if outPort != ofp.OFPP_FLOOD:
-			match = ofp_parser.OFPMatch(in_port=inPort, dl_dst=destinationMAC)
-			mod = ofp_parser.OFPFlowMod(datapath=dp, priority=1, match=match, actions=actions)
-			dp.send_msg(mod)
+        # learn a mac address to avoid FLOOD next time.
+        self.mac_to_port[dpid][src] = msg.in_port
 
-		if msg.buffer_id == ofp.OFP_NO_BUFFER:
-			data = msg.data
-		else:
-			data = None
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
 
-		out = ofp_parser.OFPPacketOut(datapath=dp, buffer_id=msg.buffer_id, in_port=msg.in_port, actions=actions, data=data)
+        actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
 
-		dp.send_msg(out)
+        # install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            self.add_flow(datapath, msg.in_port, dst, actions)
+
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = datapath.ofproto_parser.OFPPacketOut(
+            datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.in_port,
+            actions=actions, data=data)
+        datapath.send_msg(out)
+
+    @set_ev_cls(ofp_event.EventOFPPortStatus, MAIN_DISPATCHER)
+    def _port_status_handler(self, ev):
+        msg = ev.msg
+        reason = msg.reason
+        port_no = msg.desc.port_no
+
+        ofproto = msg.datapath.ofproto
+        if reason == ofproto.OFPPR_ADD:
+            self.logger.info("port added %s", port_no)
+        elif reason == ofproto.OFPPR_DELETE:
+            self.logger.info("port deleted %s", port_no)
+        elif reason == ofproto.OFPPR_MODIFY:
+            self.logger.info("port modified %s", port_no)
+        else:
+            self.logger.info("Illeagal port state %s %s", port_no, reason)

6d5d04ba42e9b38b8d440cdda7a7b9142b447d52
Wassapon Watanakeesuntorn <wassapon.w@gmail.com>
2022-02-15 14:38:09 +0900

    Update pre process script

--- Security Analysis/pre-process.py
+++ Security Analysis/pre-process.py
@@ -1,100 +1,182 @@
 from scapy.all import *
 
+import sys
 import time
 import glob, os
 
 import pandas as pd
 import numpy as np
 
-current_time = 0
+def pcap_process(pcap_name, csv_name):
+    packets = PcapReader(pcap_name)
+    with open(csv_name, 'w') as f:
+        print("Time,Size,Ether.src,Ether.dst,IP.len,IP.proto,TCP.flags,IP.src,IP.dst,IP.sport,IP.dport", file=f)
+        for i, p in enumerate(packets):
+            # print(i)
+            if IP in p:
+                print(int(p.time), file=f, end=",") # Second
+                print(len(p), file=f, end=",")
+                print(p[Ether].src, file=f, end=",")
+                print(p[Ether].dst, file=f, end=",")
+                print(p[IP].len, file=f, end=",")
+                if p[IP].proto == 6:
+                    print("TCP", file=f, end=",")
+                elif p[IP].proto == 1:
+                    print("ICMP", file=f, end=",")
+                elif p[IP].proto == 17:
+                    print("UDP", file=f, end=",")
+                if p[IP].proto == 6:
+                    print(p[TCP].flags, file=f, end=",")
+                else:
+                    print("", file=f, end=",")
+                print(p[IP].src, file=f, end=",")
+                print(p[IP].dst, file=f, end=",")
+                if (p[IP].proto == 6 or p[IP].proto == 17):
+                    try:
+                        print(p[IP].sport, file=f, end=",")
+                        print(p[IP].dport, file=f)
+                    except:
+                        print("", file=f, end=",")
+                        print("", file=f)
+                else:
+                    print("", file=f, end=",")
+                    print("", file=f)
 
-throughput = {}
-number_of_packet = {}
-average_packet_size = {}
-src_ip = {}
-src_port = {}
-dst_ip = {}
-dst_port = {}
+def clear_values():
+    global packets_count, throughput, proto_set, proto_count_TCP, proto_count_UDP, proto_count_ICMP
+    global flags_set, flags_count_PA, flags_count_FPA, flags_count_S, flags_count_SA, flags_count_A, flags_count_FA
+    global IP_src_set, IP_dst_set, IP_sport_set, IP_dport_set, ddos_flag
+    packets_count = 0
+    throughput = 0
+    proto_set = set()
+    proto_count_TCP = 0
+    proto_count_UDP = 0
+    proto_count_ICMP = 0
+    flags_set = set()
+    flags_count_PA = 0
+    flags_count_FPA = 0
+    flags_count_S = 0
+    flags_count_SA = 0
+    flags_count_A = 0
+    flags_count_FA = 0
+    IP_src_set = set()
+    IP_dst_set = set()
+    IP_sport_set = set()
+    IP_dport_set = set()
+    ddos_flag = 0
 
-tcp_flag = {}
+def packet_counter(csv_name, ts_name):
+    df = pd.read_csv(csv_name, skipinitialspace=True)
 
-table_src_ip = []
-table_src_port = []
-table_dst_ip = []
-table_dst_port = []
+    global packets_count, throughput, proto_set, proto_count_TCP, proto_count_UDP, proto_count_ICMP
+    global flags_set, flags_count_PA, flags_count_FPA, flags_count_S, flags_count_SA, flags_count_A, flags_count_FA
+    global IP_src_set, IP_dst_set, IP_sport_set, IP_dport_set, ddos_flag
+    clear_values()
+    next_time = df["Time"][0] + 1
 
-pcap_dir = "/Users/boom/Desktop/data/"
-os.chdir(pcap_dir)
-for pcap_file in glob.glob("*.pcap"):
-    pcap_data = PcapReader(pcap_dir + pcap_file)
+    with open(ts_name, 'w') as f:
+        print("Time,throughput,packets_count,avg_size,proto_set,proto_count_TCP,proto_count_UDP,proto_count_ICMP,flags_set,flags_count_PA,flags_count_FPA,flags_count_S,flags_count_SA,flags_count_A,flags_count_FA,IP_src_set,IP_dst_set,IP_sport_set,IP_dport_set", file=f)
 
-    for index, packet in enumerate(pcap_data):
-        if int(packet.time) > current_time:
-            current_time = int(packet.time)
-            throughput[current_time] = 0
-            number_of_packet[current_time] = 0
+    # for Time,IP_proto,TCP_flag,IP_src,IP_dst,IP_sport,IP_dport,Event in zip(df["Time"],df["IP.proto"],df["TCP.flags"],df["IP.src"],df["IP.dst"],df["IP.sport"],df["IP.dport"],df["Event"]):
+    for Time, Size, IP_proto, TCP_flag, IP_src, IP_dst, IP_sport, IP_dport in zip(df["Time"], df["Size"], df["IP.proto"], df["TCP.flags"], df["IP.src"], df["IP.dst"], df["IP.sport"], df["IP.dport"]):
+        # print(Time)
+        if(Time > next_time):
+            with open(ts_name, 'a') as f:
+                print(Time, file=f, end=",")
+                print(throughput, file=f, end=",")
+                print(packets_count, file=f, end=",")
+                print(throughput/packets_count, file=f, end=",")
+                print(len(proto_set), file=f, end=",")
+                print(proto_count_TCP, file=f, end=",")
+                print(proto_count_UDP, file=f, end=",")
+                print(proto_count_ICMP, file=f, end=",")
+                print(len(flags_set), file=f, end=",")
+                print(flags_count_PA, file=f, end=",")
+                print(flags_count_FPA, file=f, end=",")
+                print(flags_count_S, file=f, end=",")
+                print(flags_count_SA, file=f, end=",")
+                print(flags_count_A, file=f, end=",")
+                print(flags_count_FA, file=f, end=",")
+                print(len(IP_src_set), file=f, end=",")
+                print(len(IP_dst_set), file=f, end=",")
+                print(len(IP_sport_set), file=f, end=",")
+                print(len(IP_dport_set), file=f)
+                # print(ddos_flag)
+            clear_values()
+            next_time += 1
 
-            src_ip[current_time] = 0
-            src_port[current_time] = 0
-            dst_ip[current_time] = 0
-            dst_port[current_time] = 0
+        throughput += int(Size)
+        packets_count += 1
+        proto_set.add(IP_proto)
+        if(IP_proto=="TCP"):
+            proto_count_TCP += 1
+        if(IP_proto=="UDP"):
+            proto_count_UDP += 1
+        if(IP_proto=="ICMP"):
+            proto_count_ICMP += 1
+        flags_set.add(TCP_flag)
+        if(TCP_flag=="PA"):
+            flags_count_PA += 1
+        if(TCP_flag=="FPA"):
+            flags_count_FPA += 1
+        if(TCP_flag=="S"):
+            flags_count_S += 1
+        if(TCP_flag=="SA"):
+            flags_count_SA += 1
+        if(TCP_flag=="A"):
+            flags_count_A += 1
+        if(TCP_flag=="FA"):
+            flags_count_FA += 1
+        IP_src_set.add(IP_src)
+        IP_dst_set.add(IP_dst)
+        IP_sport_set.add(IP_sport)
+        IP_dport_set.add(IP_dport)
+        # if(Event=="ddos"):
+        # 	ddos_flag=1
 
-            tcp_flag[current_time] = {}
+def sum_file():
+    output_file = '/work/wassapon-w/data/network/DARPA_Scalable_Network_Monitoring-20091103/ts_output.csv'
 
-            table_src_ip = []
-            table_src_port = []
-            table_dst_ip = []
-            table_dst_port = []
+    with open(output_file, 'w') as f:
+        print("Time,throughput,packets_count,avg_size,proto_set,proto_count_TCP,proto_count_UDP,proto_count_ICMP,flags_set,flags_count_PA,flags_count_FPA,flags_count_S,flags_count_SA,flags_count_A,flags_count_FA,IP_src_set,IP_dst_set,IP_sport_set,IP_dport_set", file=f)
 
-        # Count throughput
-        throughput[current_time] += len(packet) # Byte
-        number_of_packet[current_time] += 1
+    for i in range(1,11):
+        folder = glob.glob("/work/wassapon-w/data/network/DARPA_Scalable_Network_Monitoring-20091103/set"+str(i)+"/ts/*.csv") 
+        for file in folder:
+            df = pd.read_csv(file, skipinitialspace=True, header=0) 
+            df.to_csv(output_file, mode='a', index=False, header=False)
 
-        # Count unique IP/port and number of packet of each TCP port
-        if IP in packet:
-            if packet[IP].src not in table_src_ip:
-                table_src_ip.append(packet[IP].src)
-                src_ip[current_time] += 1
-            if packet[IP].sport not in table_src_port:
-                table_src_port.append(packet[IP].sport)
-                src_port[current_time] += 1
-            if packet[IP].dst not in table_dst_ip:
-                table_dst_ip.append(packet[IP].dst)
-                dst_ip[current_time] += 1
-            if packet[IP].dport not in table_dst_port:
-                table_dst_port.append(packet[IP].dport)
-                dst_port[current_time] += 1
-            if packet[IP].proto == 6:
-                if packet[TCP].flags not in tcp_flag[current_time].keys():
-                    tcp_flag[current_time][packet[TCP].flags] = 0
-                tcp_flag[current_time][packet[TCP].flags] += 1
-        else:
-            pass
-            # print("Found not IP packet")
+def main():
+    pcap_dir = sys.argv[1]
+    csv_dir = sys.argv[1] + "csv/"
+    ts_dir = sys.argv[1] + "ts/"
 
-# Calculate average packet size
-for current_time in number_of_packet.keys():
-    average_packet_size[current_time] = throughput[current_time] / number_of_packet[current_time]
+    print("Directory: " + str(sys.argv[1]))
+    # print("File Number: " + str(sys.argv[2]) + " to " + str(sys.argv[3]))
 
-# print(throughput)
-# print(src_ip)
-# print(src_port)
-# print(dst_ip)
-# print(dst_port)
-# print(tcp_flag)
-# print(average_packet_size)
+    os.chdir(pcap_dir)
+    pcap_file = glob.glob("*.pcap")
 
-# Export to CSV
-throughput_df = pd.DataFrame.from_dict(throughput, orient='index', columns=['throughput'])
-number_of_packet_df = pd.DataFrame.from_dict(number_of_packet, orient='index', columns=['number_of_packet'])
-average_packet_size_df = pd.DataFrame.from_dict(average_packet_size, orient='index', columns=['average_packet_size'])
-src_ip_df = pd.DataFrame.from_dict(src_ip, orient='index', columns=['src_ip'])
-src_port_df = pd.DataFrame.from_dict(src_port, orient='index', columns=['src_port'])
-dst_ip_df = pd.DataFrame.from_dict(dst_ip, orient='index', columns=['dst_ip'])
-dst_port_df = pd.DataFrame.from_dict(dst_port, orient='index', columns=['dst_port'])
-tcp_flag_df = pd.DataFrame.from_dict(tcp_flag, orient='index')
+    # for i in range(int(sys.argv[2]), int(sys.argv[3])):
+    #     print(str(i+1) + " / " + str(len(pcap_file)) + ": " + pcap_file[i])
 
-dataset = pd.concat([throughput_df, number_of_packet_df, average_packet_size_df, src_ip_df, src_port_df, dst_ip_df, dst_port_df, tcp_flag_df], axis=1, join='inner')
-dataset.fillna(0).apply(np.int64)
+    #     pcap_name = pcap_dir + pcap_file[i]
+    #     csv_name = csv_dir + pcap_file[i] + ".csv"
+    #     pcap_process(pcap_name, csv_name)
 
-dataset.to_csv('/Users/boom/Desktop/data.csv')
+    #     ts_name = ts_dir + "ts_" + pcap_file[i] + ".csv"
+    #     packet_counter(csv_name, ts_name)
+
+    i = int(sys.argv[2]) - 1
+
+    print(str(i+1) + " / " + str(len(pcap_file)) + ": " + pcap_file[i])
+
+    pcap_name = pcap_dir + pcap_file[i]
+    csv_name = csv_dir + pcap_file[i] + ".csv"
+    pcap_process(pcap_name, csv_name)
+
+    ts_name = ts_dir + "ts_" + pcap_file[i] + ".csv"
+    packet_counter(csv_name, ts_name)
+
+if __name__ == "__main__":
+    main()

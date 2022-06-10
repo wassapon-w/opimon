c4dd6332adeac6b8804b1e125d6c238c07012d29
Wassapon Watanakeesuntorn <wassapon.w@gmail.com>
2022-06-10 22:56:41 +0700

    Update running scripts

--- Security Analysis/auto_regression.py
+++ Security Analysis/auto_regression.py
@@ -6,31 +6,38 @@ from sklearn.metrics import mean_squared_error
 from math import sqrt
 
 target_list = ["throughput", "packets_count", "avg_size", "proto_set", "proto_count_TCP", "proto_count_UDP", "proto_count_ICMP", "flags_set", "flags_count_PA", "flags_count_FPA", "flags_count_S", "flags_count_SA", "flags_count_A", "flags_count_FA", "IP_src_set", "IP_dst_set", "IP_sport_set", "IP_dport_set"]
-lags = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190]
+# lags = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190]
+tp = 1
 
-print("method,target,lags,Time(Second)")
+print("method,target,E,tau,Tp,Time(Second)")
 
 for t in range(0, 18):
-    for l in range(0, 19):
-        start = time.time()
+    for e in range(1, 21):
+        for tau in range(1,11):
+            lags = (e-1)*tau
+            if lags <= 0:
+                lags = 1
 
-        time_series = pd.read_csv('/work/wassapon-w/darpa_ts/ts_output_day1_norm.csv', header=0, index_col=0)
-        time_series_ddos = pd.read_csv('/work/wassapon-w/darpa_ts/ts_ddos_output_day1_norm.csv', header=0, index_col=0)
+            start = time.time()
 
-        steps = 27817
-        data_train = time_series[:-steps]
-        data_test  = time_series_ddos[-steps:]
+            time_series = pd.read_csv('/work/wassapon-w/darpa_ts/ts_output_day1_norm.csv', header=0, index_col=0)
+            time_series_ddos = pd.read_csv('/work/wassapon-w/darpa_ts/ts_ddos_output_day1_norm.csv', header=0, index_col=0)
 
-        model = AutoReg(data_train[target_list[t]], lags=lags[l])
-        model_fit = model.fit()
-        predictions = model_fit.predict(start=len(data_train[target_list[t]]), end=len(data_train[target_list[t]])+len(data_test[target_list[t]])-1, dynamic=False)
+            steps = 27817
+            data_train = time_series[:-steps]
+            data_test  = time_series[-steps:]
+            # data_test  = time_series_ddos[-steps:]
 
-        output = pd.DataFrame()
-        output = pd.concat([data_test[target_list[t]], predictions.to_frame().set_index(data_test.index)], axis=1)
-        output.columns = ["Observations", "Predictions"]
+            model = AutoReg(data_train[target_list[t]], lags=lags)
+            model_fit = model.fit()
+            predictions = model_fit.predict(start=len(data_train[target_list[t]]), end=len(data_train[target_list[t]])+len(data_test[target_list[t]])-1, dynamic=False)
 
-        # output.to_csv('/work/wassapon-w/network_output/ts_output_day1_auto_regression_'+target_list[t]+'_'+str(lags[l])+'.csv', index=True, header=True)
-        output.to_csv('/work/wassapon-w/network_ddos_output/ts_ddos_output_day1_auto_regression_'+target_list[t]+'_'+str(lags[l])+'.csv', index=True, header=True)
+            output = pd.DataFrame()
+            output = pd.concat([data_test[target_list[t]], predictions.to_frame().set_index(data_test.index)], axis=1)
+            output.columns = ["Observations", "Predictions"]
 
-        end = time.time()
-        print("AutoReg," + target_list[t] + "," + str(lags[l]) + "," + str(end - start))
+            output.to_csv('/work/wassapon-w/network_output/autoreg/ts_output_day1_autoreg_'+target_list[t]+"_E"+str(e)+"_tau"+str(tau)+"_tp"+str(tp)+'.csv', index=True, header=True)
+            # output.to_csv('/work/wassapon-w/network_ddos_output/autoreg/ts_ddos_output_day1_autoreg_'+target_list[t]+"_E"+str(e)+"_tau"+str(tau)+"_tp"+str(tp)+'.csv', index=True, header=True)
+
+            end = time.time()
+            print("AutoReg," + target_list[t] + "," + str(e) + "," + str(tau) + "," + str(tp) + "," + str(end - start))
--- Security Analysis/lstm.py
+++ Security Analysis/lstm.py
@@ -7,7 +7,9 @@ from keras.layers import LSTM
 from keras.layers import Dense
 
 target_list = ["throughput", "packets_count", "avg_size", "proto_set", "proto_count_TCP", "proto_count_UDP", "proto_count_ICMP", "flags_set", "flags_count_PA", "flags_count_FPA", "flags_count_S", "flags_count_SA", "flags_count_A", "flags_count_FA", "IP_src_set", "IP_dst_set", "IP_sport_set", "IP_dport_set"]
-lags = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190]
+# lags = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190]
+
+tp = 1
 
 def split_sequence(sequence, n_steps):
 	X, y = list(), list()
@@ -20,41 +22,47 @@ def split_sequence(sequence, n_steps):
 		y.append(seq_y)
 	return array(X), array(y)
 
-print("method,target,lags,Time(Second)")
+print("method,target,E,tau,Tp,Time(Second)")
 
 for t in range(0, 18):
-    for l in range(0, 19):
-        start = time.time()
+    for e in range(1, 21):
+        for tau in range(1,11):
+            lags = (e-1)*tau
+            if lags <= 0:
+                lags = 1
 
-        time_series = pd.read_csv('/work/wassapon-w/darpa_ts/ts_output_day1_norm.csv', header=0, index_col=0)
-        time_series_ddos = pd.read_csv('/work/wassapon-w/darpa_ts/ts_ddos_output_day1_norm.csv', header=0, index_col=0)
+            start = time.time()
 
-        steps = 27817
-        data_train = time_series[:-steps]
-        data_test  = time_series_ddos[-steps:]
+            time_series = pd.read_csv('/work/wassapon-w/darpa_ts/ts_output_day1_norm.csv', header=0, index_col=0)
+            time_series_ddos = pd.read_csv('/work/wassapon-w/darpa_ts/ts_ddos_output_day1_norm.csv', header=0, index_col=0)
 
-        n_steps = lags[l]
-        x_train, y_train = split_sequence(data_train[target_list[t]].reset_index(drop=True).to_numpy(), n_steps)
+            steps = 27817
+            data_train = time_series[:-steps]
+            data_test  = time_series[-steps:]
+            # data_test  = time_series_ddos[-steps:]
 
-        n_features = 1
-        x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], n_features))
+            n_steps = lags
+            x_train, y_train = split_sequence(data_train[target_list[t]].reset_index(drop=True).to_numpy(), n_steps)
 
-        model = Sequential()
-        model.add(LSTM(50, activation='relu', input_shape=(n_steps, n_features)))
-        model.add(Dense(1))
-        model.compile(optimizer='adam', loss='mse')
-        model.fit(x_train, y_train, epochs=1, verbose=0)
+            n_features = 1
+            x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], n_features))
 
-        # x_input = data_test[target_list[t]].reset_index(drop=True).to_numpy().reshape((1, n_steps, n_features))
-        x_test, y_test = split_sequence(data_test[target_list[t]].reset_index(drop=True).to_numpy(), n_steps)
-        predictions = model.predict(x_test, verbose=0)
+            model = Sequential()
+            model.add(LSTM(50, activation='relu', input_shape=(n_steps, n_features)))
+            model.add(Dense(1))
+            model.compile(optimizer='adam', loss='mse')
+            model.fit(x_train, y_train, epochs=1, verbose=0)
 
-        output = pd.DataFrame()
-        output = pd.concat([data_test[target_list[t]][n_steps:], pd.DataFrame(predictions).set_index(data_test[n_steps:].index)], axis=1)
-        output.columns = ["Observations", "Predictions"]
+            # x_input = data_test[target_list[t]].reset_index(drop=True).to_numpy().reshape((1, n_steps, n_features))
+            x_test, y_test = split_sequence(data_test[target_list[t]].reset_index(drop=True).to_numpy(), n_steps)
+            predictions = model.predict(x_test, verbose=0)
 
-        # output.to_csv('/work/wassapon-w/network_output/ts_output_day1_lstm_'+target_list[t]+'_'+str(n_steps)+'.csv', index=True, header=True)
-        output.to_csv('/work/wassapon-w/network_ddos_output/ts_ddos_output_day1_lstm_'+target_list[t]+'_'+str(n_steps)+'.csv', index=True, header=True)
+            output = pd.DataFrame()
+            output = pd.concat([data_test[target_list[t]][n_steps:], pd.DataFrame(predictions).set_index(data_test[n_steps:].index)], axis=1)
+            output.columns = ["Observations", "Predictions"]
 
-        end = time.time()
-        print("LSTM," + target_list[t] + "," + str(n_steps) + "," + str(end - start))
+            output.to_csv('/work/wassapon-w/network_output/lstm/ts_output_day1_lstm_'+target_list[t]+"_E"+str(e)+"_tau"+str(tau)+"_tp"+str(tp)+'.csv', index=True, header=True)
+            # output.to_csv('/work/wassapon-w/network_ddos_output/lstm/ts_ddos_output_day1_lstm_'+target_list[t]+"_E"+str(e)+"_tau"+str(tau)+"_tp"+str(tp)+'.csv', index=True, header=True)
+
+            end = time.time()
+            print("LSTM," + target_list[t] + "," + str(e) + "," + str(tau) + "," + str(tp) + "," + str(end - start))
